5d9bad7ede34e5ca98fb381c9c77be480a118eab
Swikarya Dahal <swikarya.dahal305@gmail.com>
2022-11-12 23:43:47 +0545

    Initial Build

---
+++ precache-manifest.fd38668335767925c3856b4f7e0dd7e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a4e7e563052b3570bf",
+    "url": "/css/app.00fbfc0b.css"
+  },
+  {
+    "revision": "74c368feb54d44eed119",
+    "url": "/css/chunk-vendors.337e1d17.css"
+  },
+  {
+    "revision": "ca030736c8590602d18a376b78f4a7c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95a4e7e563052b3570bf",
+    "url": "/js/app.ea735c00.js"
+  },
+  {
+    "revision": "74c368feb54d44eed119",
+    "url": "/js/chunk-vendors.cdfee552.js"
+  },
+  {
+    "revision": "2f583cabad6c530167b39a4d5de85197",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
